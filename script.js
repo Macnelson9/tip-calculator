388373191f78700704618a27dd3aacbe6042db5e
Macnelson9 <michaelofatu@gmail.com>
2025-01-04 19:22:22 +0100

    Added Features I have successfully added the following features to the tip calculator: 1. The ability to use the tip percentage buttons instead of manually entering the tip percentage. 2. The ability to clear the bill amount, tip percentage, and the total using the clear button.

--- script.js
+++ script.js
@@ -16,7 +16,68 @@ const tipFunction = function (bill, tip) {
 calcBtn.addEventListener('click', tipFunction);
 
 const fiveBtn = document.getElementById('five-btn');
-fiveBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('I was clicked!!');
+fiveBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : fiveBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const tenBtn = document.getElementById('ten-btn');
+tenBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : tenBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const fifteenBtn = document.getElementById('fifteen-btn');
+fifteenBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : fifteenBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const twentyBtn = document.getElementById('twenty-btn');
+twentyBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : twentyBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const fiftyBtn = document.getElementById('fifty-btn');
+fiftyBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : fiftyBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const hundredBtn = document.getElementById('hundred-btn');
+hundredBtn.addEventListener('click', function (bill, tip) {
+  bill = billAmt.value;
+  tip = tip === Number ? tipPercentage.value : hundredBtn.dataset.percentage;
+
+  const total = bill * (1 + tip / 100);
+
+  totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+});
+
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener('click', function () {
+  billAmt.value = '';
+  tipPercentage.value = '';
+  totalValue.innerHTML = 'Total: $0.00';
 });
