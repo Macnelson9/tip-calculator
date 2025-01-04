5f2a9008aacef7c41945cbae8d6b6787ac7c9724
Macnelson9 <michaelofatu@gmail.com>
2025-01-04 18:32:05 +0100

    New commit I have made some changes to the files, trying to add some new features to the tip calculator like fixed percentages buttons, bill splitting, formatting currency based on user location. I haven't gotten the hang of it yet but I most definitely will. Happy Coding!

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const tipPercentage = document.getElementById('tip-percentage');
 const calcBtn = document.querySelector('.calc-btn');
 const totalValue = document.querySelector('.total');
 
-calcBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  const billValue = billAmt.value;
-  const tipValue = tipPercentage.value;
-
-  const total = billValue * (1 + tipValue / 100);
+const tipFunction = function (bill, tip) {
+  bill = billAmt.value;
+  tip = tipPercentage.value;
+  const total = bill * (1 + tip / 100);
 
   totalValue.innerHTML = `Total: $${total.toFixed(2)}`;
+};
+
+calcBtn.addEventListener('click', tipFunction);
+
+const fiveBtn = document.getElementById('five-btn');
+fiveBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('I was clicked!!');
 });
